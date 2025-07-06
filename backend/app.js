f7b0e51c4e57309a2128b01ad945f0c2c5d124de
saif ullah <saifullaha730@gmail.com>
2025-07-06 11:38:03 +0500

    i create server and connect ot mongodb

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("mongodb is connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
